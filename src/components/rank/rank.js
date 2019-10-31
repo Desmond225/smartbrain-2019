c741d1cf19356c2607d83d073e77a20528264f26
terefs <d.van.zurk@gmail.com>
2019-10-31 15:37:30 +0100

    Rendering box around faces based on API calculations

--- src/components/rank/rank.js
+++ src/components/rank/rank.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Rank = () => {
     return (
         <div>
-            <div className="white f3">
+            {/* <div className="white f3">
                 {'Des, your current rank.. '}
             </div>
             <div className="white f2">
                 {'#5'}
-            </div>
+            </div> */}
         </div>
     )
 }
